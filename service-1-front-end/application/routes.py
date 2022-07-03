5c86acbca1a81a8ec67bebbdcb095cbbd8b53c9f
= <liam2robertson@gmail.com>
2022-07-03 18:28:22 +0100

    implement service 4 (create palette api)

--- service-1-front-end/application/routes.py
+++ service-1-front-end/application/routes.py
@@ -6,4 +6,14 @@ import requests
 def index():
     base_color = requests.get('http://service-2:5000/get_base_color').text
     palette_type = requests.get('http://service-3:5000/get_palette_type').text
-    return render_template('home.html', base_color=base_color, palette_type=palette_type)
+    palette = requests.post(
+        'http://service-4:5000/create_palette',
+        json=dict(base_color=base_color, palette_type=palette_type)
+    ).json()
+    
+    return render_template(
+        'home.html', 
+        base_color=base_color, 
+        palette_type=palette_type,
+        colors=palette['colors']
+    )
