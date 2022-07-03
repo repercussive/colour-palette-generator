9170c03728a35cc708a335ae201cc6b4965410b3
= <liam2robertson@gmail.com>
2022-07-03 18:22:34 +0100

    implement service 2 (base colour generator)

--- service-1-front-end/application/routes.py
+++ service-1-front-end/application/routes.py
@@ -1,6 +1,8 @@
 from application import app
 from flask import render_template
+import requests
 
 @app.route('/')
 def index():
-    return render_template('home.html') 
+    base_color = requests.get('http://service-2:5000/get_base_color').text
+    return render_template('home.html', base_color=base_color) 
