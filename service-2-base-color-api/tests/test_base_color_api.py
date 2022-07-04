c20beef69f382d6b1c33ca602db27c8bb81ee986
= <liam2robertson@gmail.com>
2022-07-04 11:44:45 +0100

    add unit tests for service 2

---
+++ service-2-base-color-api/tests/test_base_color_api.py
@@ -0,0 +1,17 @@
+from application import app
+from flask import url_for
+from flask_testing import TestCase
+
+class TestBase(TestCase):
+  def create_app(self):
+      return app
+
+class TestBaseColorApi(TestBase):
+  def test_get_base_color(self):
+      response = self.client.get(url_for('get_base_color'))
+      color = response.data.decode('utf-8')
+      hex = color.split('#')[1]
+      assert color[0] == '#'
+      assert hex.isalnum() == True
+      assert len(hex) == 6
+
