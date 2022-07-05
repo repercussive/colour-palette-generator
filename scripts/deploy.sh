01d68dee1e0934a1b1859b06c69088679687bf02
Liam Robertson <liam2robertson@gmail.com>
2022-07-05 21:36:33 +0000

    reduce ansible verbosity

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 scp docker-compose.yaml 10.182.0.2:/home/jenkins/docker-compose.yaml
 scp nginx.conf 10.182.0.2:/home/jenkins/nginx.conf
-ansible-playbook -i ansible/inventory.yaml ansible/playbook.yaml -vvvv
+ansible-playbook -i ansible/inventory.yaml ansible/playbook.yaml -v
